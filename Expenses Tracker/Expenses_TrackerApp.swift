0f88f7ac06ede917197e4b70cae2d4900392886e
aabdyshev <aabdyshev@devcats.kg>
2026-02-09 12:44:15 +0600

    Initial Commit

---
+++ Expenses Tracker/Expenses_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expenses_TrackerApp.swift
+//  Expenses Tracker
+//
+//  Created by Amantay Abdyshev on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Expenses_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
