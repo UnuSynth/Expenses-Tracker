d4dbad5870c74f2f5bfca613fa2ed756f30fb01d
amantai.abdyshev <amantai.abdyshev@outlook.com>
2026-02-11 14:44:30 +0600

    init commit

---
+++ UI/GeneralScreen.swift
@@ -0,0 +1,22 @@
+//
+//  GeneralScreen.swift
+//  Expenses Tracker
+//
+//  Created by Amantay Abdyshev on 9/2/26.
+//
+
+import SwiftUI
+
+struct GeneralScreen: View {
+    var body: some View {
+        
+    }
+}
+
+extension GeneralScreen {
+    
+}
+
+#Preview {
+    GeneralScreen()
+}
