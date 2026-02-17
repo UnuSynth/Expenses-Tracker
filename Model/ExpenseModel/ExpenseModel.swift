cec8d110a855709b5f31710fc4e88cb36ee94bf8
amantai.abdyshev <amantai.abdyshev@outlook.com>
2026-02-17 20:44:58 +0600

    in-progress: implement-add-expense-screen

--- Model/ExpenseModel/ExpenseModel.swift
+++ Model/ExpenseModel/ExpenseModel.swift
@@ -4,17 +4,41 @@
 //
 //  Created by Amantay Abdyshev on 17/2/26.
 //
+import Foundation
 
-struct Expense {
+struct ExpenseModel {
     let date: Date
     let amount: Double
     let category: String
-    let notes: ExpenseNotes?
+    let notes: Notes?
 }
 
-extension Expense {
-    struct ExpenseNotes {
+
+extension ExpenseModel {
+    enum Category: String, CaseIterable, Identifiable {
+        case groceries
+        case lunch
+        case clothes
+        case sport
+        
+        var id: String { self.rawValue }
+        
+        var displayName: String {
+            switch self {
+            case .groceries:
+                return "🛒 Groceries"
+            case .lunch:
+                return "🍽️ Lunch"
+            case .clothes:
+                return "👗 Clothes"
+            case .sport:
+                return "🏀 Sport"
+            }
+        }
+    }
+    
+    struct Notes {
         let description: String?
-        let image: Image?
+        let image: String?
     }
 }
--- UI/AddExpense/AddExpenseView.swift
+++ UI/AddExpense/AddExpenseView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct AddExpenseView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var date: Date = .now
+    @State private var time: Date = .now
+    @State private var amount: String = ""
+    @State private var category: ExpenseModel.Category = .groceries
+    @State private var notes: String = ""
+    
     var body: some View {
-        
+        NavigationStack {
+            form.toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(role: .close) {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .confirmationAction) {
+                    Button(role: .confirm) {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+    
+    private var form: some View {
+        Form {
+            DatePicker(
+                "Date",
+                selection: $date,
+                displayedComponents: .date
+            )
+            
+            DatePicker(
+                "Time",
+                selection: $time,
+                displayedComponents: .hourAndMinute
+            )
+            
+            LabeledContent {
+                TextField("", text: $amount)
+                    .keyboardType(.decimalPad)
+                    .multilineTextAlignment(.trailing)
+            } label: {
+                Text("Amount")
+            }
+            
+            Picker("Category", selection: $category) {
+                ForEach(ExpenseModel.Category.allCases) { category in
+                    Text(category.displayName)
+                        .tag(category)
+                }
+            }
+            .pickerStyle(.automatic)
+            
+            LabeledContent {
+                TextField("", text: $notes)
+                    .multilineTextAlignment(.trailing)
+                    .lineLimit(5...10)
+            } label: {
+                Text("Notes")
+            }
+        }
     }
 }
 
