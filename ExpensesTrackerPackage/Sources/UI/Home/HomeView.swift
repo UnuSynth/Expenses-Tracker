e1ceb7383b513d0da777e3e4a785d1306b99e748
amantai.abdyshev <saidarov74@gmail.com>
2026-05-28 16:16:57 +0600

    Update HomeView.swift

--- ExpensesTrackerPackage/Sources/UI/Home/HomeView.swift
+++ ExpensesTrackerPackage/Sources/UI/Home/HomeView.swift
@@ -48,7 +48,7 @@ struct HomeView: View {
                                 }
                             }
                             .background(.white)
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
                             .padding(.bottom, 8)
                         } header: {
                             ExpenseListSectionHeader(
